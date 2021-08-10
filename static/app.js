6fa37d080c2ca69e3605c8cc708576ff38dfbea6
Nicolas Acquaviva <nicolaseacquaviva@gmail.com>
2021-08-10 21:20:21 +0000

    ref(static): remove app.js file

--- static/app.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-let photo = document.getElementById("image-file").files[0];
-let formData = new FormData();
-
-formData.append("photo", photo);
-fetch('/upload/image', {method: "POST", body: formData});
